a0908aeece9fad6f42f57e4774cab11f18fe8afd
Akshay Jadhav <jadhavakshay4124@gmail.com>
2020-08-14 15:00:39 +0530

    Add project files.

---
+++ AssesmentTokenBased/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AssesmentTokenBased.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ AssesmentTokenBased/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using AssesmentTokenBased.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AssesmentTokenBased.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> get()
+        {
+            using(EmployeesEntities db=new EmployeesEntities())
+            {
+                return db.Employees.ToList();
+            }
+        }
+    }
+}
